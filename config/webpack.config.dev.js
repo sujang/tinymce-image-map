5291f96a342c9d4824da54b27750c9c22ea7653f
tiberius-s <tiberius220@gmail.com>
2018-07-17 08:52:51 -0500

    Added license attribution for icon

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -29,7 +29,7 @@ module.exports = {
         use: {
           loader: "url-loader",
           options: {
-            limit: 1000
+            limit: 8000
           }
         }
       },
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -35,7 +35,7 @@ module.exports = {
         use: {
           loader: "url-loader",
           options: {
-            limit: 1000
+            limit: 8000
           }
         }
       },
